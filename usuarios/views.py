e17b57968a0d8aa43f6614ace8472a44525e4c49
JeremiasMartin <jmtnalv@gmail.com>
2023-05-04 20:06:54 -0300

    Registro de usuario genera clave aleatoria

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from usuarios.models import Cliente, Usuario
 from .models import *
 from .forms import *
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth import logout as django_logout
 from django.shortcuts import render, redirect
@@ -12,6 +11,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponseRedirect
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.shortcuts import render, redirect
+from email.mime.image import MIMEImage
+from django.template.loader import get_template
+from OhMyDog import settings
+from django.core.mail import EmailMultiAlternatives
+import os
+
 
 # Create your views here.
 
@@ -47,17 +53,16 @@ def user_logout(request):
 
 
 
-@login_required
 def registrar_cliente(request):
     if request.method == 'POST':
         form = ClienteRegistroForm(request.POST)
         if form.is_valid():
             # Crear un usuario a partir del email y la contraseña proporcionados por el cliente
+            passwd = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz0123456789', k=8))
             usuario = Usuario.objects.create_user(
                 email=form.cleaned_data.get('email'),
-                password=form.cleaned_data.get('password1')
+                password = passwd
             )
-            # Crear un cliente asociado al usuario
             cliente = Cliente.objects.create(
                 user=usuario,
                 dni=form.cleaned_data.get('dni'),
@@ -65,12 +70,39 @@ def registrar_cliente(request):
                 apellido=form.cleaned_data.get('apellido'),
                 telefono=form.cleaned_data.get('telefono')
             )
-            # Redirigir a la lista de clientes
-            
-            return redirect("/")
+            subject = 'Registro de Usuario Exitoso'
+            from_email = 'Ejtech <%s>' % (settings.EMAIL_HOST_USER)
+            to_email = '%s' % (form.cleaned_data.get('email'))
+            reply_to_email = 'noreply@ejtechsoft.com'
+
+            image_dir = os.path.join(settings.BASE_DIR, 'OhMyDogApp', 'static', 'OhMyDogApp', 'img')
+            image_name = 'logo.png'
+
+            context = {
+                    'nombre' : form.cleaned_data.get('nombre'),
+                    'password'  : passwd
+                    }
+            text_content = get_template('cliente/mail_bienvenida.txt')
+            html_content = get_template('cliente/mail_bienvenida.html')
+            text_content = text_content.render(context)
+            html_content = html_content.render(context)
+
+            email = EmailMultiAlternatives(subject, text_content, from_email, to=[to_email,], reply_to=[reply_to_email,])
+            email.mixed_subtype = 'related'
+            email.content_subtype = 'html'
+            email.attach_alternative(html_content, 'text/html')
+
+            file_path = os.path.join(image_dir, image_name)
+            with open(file_path, 'rb') as f:
+                image = MIMEImage(f.read())
+                image.add_header('Content-ID', '<%s>' % (image_name))
+                image.add_header('Content-Disposition', 'inline', filename=image_name)
+                email.attach(image)
+
+            email.send(fail_silently=False)
+            return redirect("/usuarios/clientes")
     else:
         form = ClienteRegistroForm()
-
     context = {'form': form}
     return render(request, 'admin/registrar_cliente.html',context)
 
