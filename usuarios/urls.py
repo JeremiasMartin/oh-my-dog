ffe30cc210b175f9f4a81e00b7903c9b86a63b6e
JeremiasMartin <jmtnalv@gmail.com>
2023-05-06 15:47:20 -0300

    Función de cambiar contraseña agregada

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('restablecer_contrasenia_enviado/',restDone, name='restablecer_contrasenia_enviado'),
     path('reset/<uidb64>/<token>', restPasswordConfirm.as_view(template_name='cliente/rest-contra-conf.html'), name='password_reset_confirm'),
     path('reset_password_complete/', views.PasswordResetCompleteView.as_view(template_name="cliente/restablecer_contrasenia_exitoso.html"), name='password_reset_complete'),
+    path('cambiar_contrasenia/', cambiar_contrasenia.as_view(template_name='cliente/cambiar_contrasenia.html')),
+    path('password_change/done/',views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
