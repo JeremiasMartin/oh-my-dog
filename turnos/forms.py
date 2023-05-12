a1f0c83f7bfb20dcd070d22ecedd60880b9a9331
TomasPalacios223 <80858468+TomasPalacios223@users.noreply.github.com>
2023-05-12 14:07:24 -0300

    solicitarTurno terminado

--- turnos/forms.py
+++ turnos/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import Turno
+from django import forms
+from django.forms.widgets import DateInput
+from datetime import date
+
 
 class SolicitarTurnoForm(forms.ModelForm):
     class Meta:
         model = Turno
         fields = ['fecha', 'tipo_atencion', 'estado']
+        widgets = {
+            'fecha': DateInput(attrs={'type': 'date','min': str(date.today())})
+        }
+        
+        
