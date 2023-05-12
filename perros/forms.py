ed17b16f6b33ae3b65fb902f5176cb87e93c33e0
emrome <m.emiliaromero4@gmail.com>
2023-05-12 10:39:26 -0300

    Comprobacion en campo peso de atencion

--- perros/forms.py
+++ perros/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Perro, Atencion, Tipo_atencion
-from django.core.validators import RegexValidator
+from django.core.validators import RegexValidator, MinValueValidator
 
 class registrar_perro(forms.ModelForm):
     nombre = forms.CharField(validators=[RegexValidator((r'^[a-zA-Z ]+$'), message="Por favor ingrese solo letras sin acentos.")])
@@ -20,7 +20,7 @@ class editar_pefil_mascota(forms.ModelForm):
 
 class registrar_atencion_form(forms.ModelForm):
     tipo = forms.ModelChoiceField(queryset=Tipo_atencion.objects.all(), empty_label=None)
-
+    peso = forms.DecimalField(validators=[MinValueValidator(0)])
     class Meta:
         model = Atencion
         fields = ['tipo', 'peso', 'observacion']
