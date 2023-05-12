cec2d069c528dcde4dd3336377fb377714d7d592
JeremiasMartin <jmtnalv@gmail.com>
2023-05-12 16:12:13 -0300

    View de Solicitar Turno

--- turnos/views.py
+++ turnos/views.py
@@ -3,18 +3,21 @@ from .forms import *
 from django.utils.timezone import datetime
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 # Create your views here.
 
 @login_required
 def solicitar_turno(request):
     if request.method == 'POST':
-        form =SolicitarTurnoForm(request.POST)
+        form = SolicitarTurnoForm(request.POST)
         if form.is_valid():
             turno = form.save(commit=False)
-            turno.user = request.user
-            turno.estado = 'pendiente'       
-            turno.save()                 
-            return redirect('ver_turnos')
+            turno.cliente = request.user.cliente
+            turno.estado_id = 3
+            turno.save()
+            messages.success(request, 'Turno solicitado correctamente')
+            return redirect('solicitar_turno')
     else:
-        form = SolicitarTurnoForm(initial={'fecha': datetime.now()})
+        form = SolicitarTurnoForm()
     return render(request, 'solicitar_turno.html', {'form': form})
