abd90c7a10a886f1dd3c35652035cafd775e5ab1
JeremiasMartin <jmtnalv@gmail.com>
2023-05-12 20:19:23 -0300

    Restricciones de todas las atenciones

--- perros/views.py
+++ perros/views.py
@@ -4,7 +4,8 @@ from usuarios.models import Cliente
 from .forms import registrar_perro, editar_pefil_mascota, registrar_atencion_form
 from django.contrib import messages
 from django.core.paginator import Paginator
-from datetime import datetime
+from datetime import datetime, date, timedelta
+from turnos.models import Turno
 
 
 def listar_mascotas_cliente(request, cliente_id):
@@ -56,15 +57,53 @@ def editar_perfil_mascota(request, id):
 
 def registrar_atencion(request, id_mascota):
     perro = get_object_or_404(Perro, id=id_mascota)
+    tiene_error = False
+    
     if request.method == 'POST':
         form = registrar_atencion_form(request.POST)
         if form.is_valid():
             atencion = form.save(commit=False)
             atencion.mascota = perro
             atencion.fecha = datetime.now()
-            atencion.save()
-            messages.success(request, 'Atención registrada exitosamente')
-            return redirect(f'/perros/listar-mascotas-cliente/{perro.cliente.cliente_id}/')
+            
+            if atencion.tipo.tipo == 'Castración' and Atencion.objects.filter(mascota=perro, tipo__tipo='Castración').exists():
+                messages.error(request, 'La mascota ya ha recibido una atención de tipo "Castración" anteriormente')
+                tiene_error = True
+            
+            elif atencion.tipo.tipo == 'Vacuna antirrábica' and (date.today() - perro.fecha_nac).days > 120:
+                turno = Turno.objects.create(
+                    cliente_id=perro.cliente_id,
+                    tipo_atencion_id=atencion.tipo.id,
+                    fecha=date.today() + timedelta(days=365),
+                    estado_id=3,
+                )
+            
+            elif atencion.tipo.tipo == 'Vacuna antiviral' and (date.today() - perro.fecha_nac).days > 60:
+                if (date.today()-perro.fecha_nac).days > 60 and (date.today()-perro.fecha_nac).days < 120:
+                    turno = Turno.objects.create(
+                        cliente_id=perro.cliente_id,
+                        tipo_atencion_id=atencion.tipo.id,
+                        fecha=date.today() + timedelta(days=21),
+                        estado_id=3,
+                    )
+                elif (date.today()-perro.fecha_nac).days > 120:
+                    turno = Turno.objects.create(
+                        cliente_id=perro.cliente_id,
+                        tipo_atencion_id=atencion.tipo.id,
+                        fecha=date.today() + timedelta(days=365),
+                        estado_id=3,
+                    )
+            else:
+                if atencion.tipo.tipo == 'Vacuna antirrábica':
+                    messages.error(request, 'Esta mascota es menor de 4 meses, no puede recibir la vacuna antirrábica')
+                else:
+                    messages.error(request, 'Esta mascota es menor de 2 meses, no puede recibir la vacuna antiviral')
+                tiene_error = True
+            
+            if not tiene_error:
+                atencion.save()
+                messages.success(request, 'Atención registrada exitosamente')
+                return redirect(f'/perros/registrar-atencion/{perro.id}/')
     else:
         form = registrar_atencion_form()
     context={
