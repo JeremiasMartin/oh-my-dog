02516f84ec1f2c8527c0073beaa816d9e3c160d9
JeremiasMartin <jmtnalv@gmail.com>
2023-05-28 18:35:57 -0300

    Es necesario elegir una ubicación para el registro y form de edición de personal

--- servicios/forms.py
+++ servicios/forms.py
@@ -13,7 +13,40 @@ class PersonalForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['ubicacion'].required = False
+    
+    def clean_ubicacion(self):
+        ubicacion = self.cleaned_data.get('ubicacion')
+        if not ubicacion:
+            raise forms.ValidationError('Debe seleccionar una ubicación en el mapa.')
+        return ubicacion
 
+    def save(self, commit=True):
+        personal = super().save(commit=False)
+        coordenadas = self.cleaned_data.get('ubicacion')
+        if coordenadas:
+            personal.ubicacion = coordenadas
+        if commit:
+            personal.save()
+        return personal
+    
+class PersonalEditForm(forms.ModelForm):
+    
+    ubicacion = gis_forms.PointField(widget=LeafletWidget())
+
+    class Meta:
+        model = Personal
+        fields = ('nombre', 'contacto', 'tipo', 'direccion', 'horario', 'ubicacion')
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['ubicacion'].required = False
+    
+    def clean_ubicacion(self):
+        ubicacion = self.cleaned_data.get('ubicacion')
+        if not ubicacion:
+            raise forms.ValidationError('Debe seleccionar una ubicación en el mapa.')
+        return ubicacion
+    
     def save(self, commit=True):
         personal = super().save(commit=False)
         coordenadas = self.cleaned_data.get('ubicacion')
