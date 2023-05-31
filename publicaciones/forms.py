c20ca5c974f26363711a3f3790b71518c8c58fad
TomasPalacios223 <80858468+TomasPalacios223@users.noreply.github.com>
2023-05-31 15:06:36 -0300

    ultimos cambios para adopcion terminado

--- publicaciones/forms.py
+++ publicaciones/forms.py
@@ -1,13 +1,22 @@
+
 from django import forms
-from .models import Adopcion
+from .models import Perro_publicacion, Adopcion
 
 class AdopcionForm(forms.ModelForm):
+    foto = forms.ImageField(required=True)
+    origen = forms.CharField(max_length=100, widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}))
+    motivo_adopcion = forms.CharField(max_length=100, widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}))
+
     class Meta:
-        model = Adopcion
-        fields = ('perro_publicacion', 'origen', 'motivo_adopcion')
+        model = Perro_publicacion
+        fields = ('nombre', 'tamanio', 'sexo', 'color', 'edad', 'raza', 'foto')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['perro_publicacion'].required = True
-        self.fields['origen'].required = True
-        self.fields['motivo_adopcion'].required = True
+        self.fields['nombre'].required = True
+        self.fields['tamanio'].required = True
+        self.fields['sexo'].required = True
+        self.fields['color'].required = True
+        self.fields['edad'].required = True
+        self.fields['raza'].required = True
+        self.fields['foto'].required = True
