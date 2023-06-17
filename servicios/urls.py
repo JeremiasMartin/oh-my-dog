7a8275f40e9bb0eda64314339632d00c4c5a2117
JeremiasMartin <jmtnalv@gmail.com>
2023-06-17 20:17:56 -0300

    Enviar consultas a servicios de terceros

--- servicios/urls.py
+++ servicios/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('listar_personal/', views.listar_personal, name='Listar_personal'),
     path('editar_personal/<int:personal_id>/', views.editar_personal, name='Editar_personal'),
     path('cambiar_estado/<int:personal_id>/', views.cambiar_estado, name='Cambiar_estado'),
+    path('contacto/<int:personal_id>/', views.enviar_consulta, name='Contacto'),
     path('mapa/', views.mapa, name='Mapa'),
 ]
--- servicios/views.py
+++ servicios/views.py
@@ -5,6 +5,9 @@ from .forms import *
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from turnos.views import paginar
+from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
 
 def listar_personal(request):
     personal = Personal.objects.all()
@@ -34,6 +37,45 @@ def editar_personal(request, personal_id):
     context = {'form': form, 'errors': form.errors}
 
     return render(request, 'editar_personal.html', context)
+
+from django.core.mail import send_mail
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+def enviar_consulta(request, personal_id):
+    person = get_object_or_404(Personal, id=personal_id)
+
+    if request.method == 'POST':
+        consulta = request.POST.get('consulta')
+        subject = 'Consulta ¡Oh My Dog!'
+
+        if request.user.is_authenticated:
+            from_email = request.user.email
+        else:
+            from_email = request.POST.get('email')
+
+        to_email = person.email
+        reply_to_email = 'noreply@ejtechsoft.com'
+
+        context = {
+            'consulta': consulta,
+            'mail_consulta': from_email,
+        }   
+        
+        text_content = get_template('mail/consulta_mail.txt')
+        html_content = get_template('mail/consulta_mail.html')
+        text_content = text_content.render(context)
+        html_content = html_content.render(context)
+
+        email = EmailMultiAlternatives(subject, text_content, from_email, to=[to_email,], reply_to=[reply_to_email,])
+        email.mixed_subtype = 'related'
+        email.content_subtype = 'html'
+        email.attach_alternative(html_content, 'text/html')
+        email.send(fail_silently=False)
+        messages.success(request, "¡Consulta enviada correctamente!")
+        return redirect('Contacto', personal_id=personal_id)
+    else:
+        return render(request, 'enviar_consulta.html', {'person': person})
 
 
 def mapa(request):
