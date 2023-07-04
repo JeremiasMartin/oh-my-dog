42cecbd0580e89009c01fa50a9a3b2bca78da914
JeremiasMartin <jmtnalv@gmail.com>
2023-07-04 17:44:07 -0300

    Funcionalidad de donar, envío de mails y generación de comprobante, listado de donaciones

--- servicios/views.py
+++ servicios/views.py
@@ -12,6 +12,17 @@ import datetime
 import mercadopago
 from mercadopago.config import RequestOptions
 from unidecode import unidecode
+from .models import Campaña
+from .forms import DonacionForm
+import mercadopago
+from django.urls import reverse
+from urllib.parse import urlparse
+from django.db.models import F
+from OhMyDog import settings
+import tempfile
+from django.core.mail import EmailMultiAlternatives
+import xhtml2pdf.pisa as pisa
+from django.http import HttpResponse
 
 
 def listar_personal(request):
@@ -170,38 +181,136 @@ def editar_campaña(request, campaña_id):
 
 
 def listar_campañas(request):
-    campañas = Campaña.objects.all()
-    context = {"campañas":paginar(request,campañas,6), "fechaHoy":datetime.datetime.today()}
+    fechaHoy = datetime.datetime.today().date()
+    campañas = Campaña.objects.filter(fechaFin__gt=fechaHoy)
+    context = {"campañas": paginar(request, campañas, 6), "fechaHoy": fechaHoy}
     return render(request, 'listar_campañas.html', context)
 
 
-def donar(request):
-    sdk = mercadopago.SDK("TEST-2247251725354942-061914-a9d31ca1c16d102f5554d58738633dc0-1134118165")
+def donar(request, campaña_id):
+    campaña = get_object_or_404(Campaña, id=campaña_id)
+
+    if request.method == 'POST':
+        form = DonacionForm(request.POST, user=request.user)
+        if form.is_valid():
+            email = request.user.email if request.user.is_authenticated else form.cleaned_data['email']
+            monto = request.POST.get('monto')
+            preference_data = {
+                "items": [
+                    {
+                        "title": campaña.nombre,
+                        "quantity": 1,
+                        "currency_id": "ARS",
+                        "unit_price": float(monto)
+                    }
+                ],
+                "back_urls": {
+                    "success": request.build_absolute_uri(reverse('Donacion_exitosa', args=[campaña_id, monto]) + f'?email={email}'),
+                    "failure": request.build_absolute_uri(reverse('Donar', args=[campaña_id])),
+                    "pending": request.build_absolute_uri(reverse('Donar', args=[campaña_id]))
+                }
+            }
+            sdk = mercadopago.SDK("TEST-2247251725354942-061914-a9d31ca1c16d102f5554d58738633dc0-1134118165")
+            preference = sdk.preference().create(preference_data)
+            payment_url = preference['response']['sandbox_init_point']
+            return redirect(payment_url)
+    else:
+        form = DonacionForm()
+
+    return render(request, 'donar.html', {'form': form, 'campaña': campaña})
 
 
- 
-    preference_data = {
-        "items": [
-            {
-                "title": "Producto 1",
-                "quantity": 1,
-                "currency_id": "ARS",
-                "unit_price": 100.0
-            }  
-        ],
-        "back_urls": {
-        "success": "localhost:8000/",
-        "failure": "localhost:8000/",
-        "pending": "localhost:8000/"
+def donacion_exitosa(request, campaña_id, monto):
+    campaña = get_object_or_404(Campaña, id=campaña_id)
+
+    email = request.GET.get('email', '')
+
+    campaña.recaudado = F('recaudado') + int(monto)
+    campaña.save()
+
+    donacion = Donacion.objects.create(
+        campaña=campaña,
+        monto=int(monto),
+        fecha=datetime.datetime.now(),
+        nombre=request.user.nombre if request.user.is_authenticated else None,
+        apellido=request.user.apellido if request.user.is_authenticated else None,
+        email=request.user.email if request.user.is_authenticated else email,
+    )
+
+    subject = 'Donación Exitosa'
+    from_email = 'Ejtech <%s>' % (settings.EMAIL_HOST_USER)
+    to_email = request.user.email if request.user.is_authenticated else email
+    reply_to_email = 'noreply@ejtechsoft.com'
+
+
+    context = {
+        'nombre_donador': request.user.nombre if request.user.is_authenticated else None,
+        'apellido_donador': request.user.apellido if request.user.is_authenticated else None,
+        'email_donador': request.user.email if request.user.is_authenticated else email,
+        'campaña': campaña.nombre,
+        'monto': monto,
+        'fecha': donacion.fecha,
     }
-    }
+
+    text_content = get_template('mail/donacion_mail.txt')
+    html_content = get_template('mail/donacion_mail.html')
+    text_content = text_content.render(context)
+    html_content = html_content.render(context)
+
+    email = EmailMultiAlternatives(subject, text_content, from_email, to=[to_email,], reply_to=[reply_to_email,])
+    email.mixed_subtype = 'related'
+    email.content_subtype = 'html'
+    email.attach_alternative(html_content, 'text/html')
+
+    html_content = get_template('mail/comprobante_donacion.html')
+    html_content = html_content.render(context)
+    with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as pdf_file:
+        pisa_status = pisa.CreatePDF(html_content, dest=pdf_file)
+        if not pisa_status.err:
+            pdf_file.seek(0)
+            email.attach('comprobante_donacion.pdf', pdf_file.read(), 'application/pdf')
+
+    email.send(fail_silently=False)
     
+    if request.user.is_authenticated:
+        request.user.descuento = True
+        request.user.save()
+    
+    else:
+        return redirect('/')
 
-    preference = sdk.preference().create(preference_data)
+    return redirect('Listar_donaciones')
 
-    payment_url = preference['response']['sandbox_init_point']
-    print(payment_url)
-    return redirect(payment_url)
+
+def listar_donaciones(request):
+    donaciones = Donacion.objects.filter(email=request.user.email)
+    context = {"donaciones": paginar(request, donaciones, 6)}
+    return render(request, 'listar_donaciones.html', context)
+
+
+def descargar_comprobante(request, donacion_id):
+    donacion = get_object_or_404(Donacion, id=donacion_id)
+
+    context = {
+        'nombre_donador': donacion.nombre,
+        'apellido_donador': donacion.apellido,
+        'email_donador': donacion.email,
+        'campaña': donacion.campaña.nombre,
+        'monto': donacion.monto,
+        'fecha': donacion.fecha,
+    }
+
+    html_content = get_template('mail/comprobante_donacion.html')
+    html_content = html_content.render(context)
+    with tempfile.NamedTemporaryFile(suffix='.pdf', delete=False) as pdf_file:
+        pisa_status = pisa.CreatePDF(html_content, dest=pdf_file)
+        if not pisa_status.err:
+            pdf_file.seek(0)
+            response = HttpResponse(pdf_file.read(), content_type='application/pdf')
+            response['Content-Disposition'] = 'attachment; filename=comprobante_donacion.pdf'
+            return response
+
+    return redirect('Listar_donaciones')
 
 def buscar(request, filtros, personal):
     print("FILTRO",filtros)
