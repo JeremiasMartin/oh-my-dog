0a07dede8c99a4146f1b2339000c55175e7eea90
emrome <m.emiliaromero4@gmail.com>
2023-07-04 11:20:34 -0300

    Postularse en Cruza

--- cruza/models.py
+++ cruza/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+from publicaciones.models import Publicacion
 
+class Postulacion(models.Model):
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, related_name='publicacion')
+    publicacion_postulante = models.ForeignKey(Publicacion, on_delete=models.CASCADE, related_name='postulante')
+    match= models.BooleanField(default=False)
+    fecha_match = models.DateField('Fecha', blank=True, null=True)
+    
+    class Meta:
+        db_table = 'postulaciones_cruza'
